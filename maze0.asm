a1e4a604f0719afa5e79ca7a6734b58ec48670d6
Zhijin Song <145217761+JasmineSong666@users.noreply.github.com>
2024-11-24 01:51:00 +0000

    Fix bugs

--- maze0.asm
+++ maze0.asm
@@ -1,15 +1,7 @@
 .data
 
 
-myarray: .ascii "
-0000, 0000, 0000, 0000, 0000, 0100,
-0000, 1000, 0110, 1000, 1110, 0011,
-0000, 1100, 1111, 0010, 0101, 0100,
-0000, 0001, 1001, 1110, 0011, 0101,
-0000, 1100, 1110, 0111, 1000, 0111,
-0000, 0001, 0101, 1001, 1110, 0011,
-1000, 1000, 1011, 0010, 1001, 0010,
-"
+myarray: .ascii "0000, 0000, 0000, 0000, 0000, 0100, 0000, 1000, 0110, 1000, 1110, 0011, 0000, 1100, 1111, 0010, 0101, 0100, 0000, 0001, 1001, 1110, 0011, 0101, 0000, 1100, 1110, 0111, 1000, 0111, 0000, 0001, 0101, 1001, 1110, 0011, 1000, 1000, 1011, 0010, 1001, 0010,"
 # Each cell contains 4 digits of number. The 4 digits stand for 4 directions, which are "Forward,Right,Back,Left" respectively, where "0" stands for wall, "1" stands for valid path
 
 # All cells in the first row and first column are considered outside the maze, and are filled with number "0000", except for the starting point.
@@ -17,7 +9,7 @@ myarray: .ascii "
 
 mazewidth: .word 6
 ycord: .word 6 # This should be the beginning point of the robot, which is row 6. Input ranges from 0-6 (7 rows in total).
-xcord: .word 0 # This should be the beginning point of the robot, which is column 0. Input ranges from 0-5 (6 columns in total). Input above 5 will make the address jumping to next row and return wrong result.
+xcord: .word 1 # This should be the beginning point of the robot, which is column 0. Input ranges from 0-5 (6 columns in total). Input above 5 will make the address jumping to next row and return wrong result.
 
 mistakes: .word 0     # Number of mistakes
 total_moves: .word 0  # Total number of moves
@@ -44,21 +36,21 @@ main:
 
     j beginning_loop
 
-## loops until user enters maze
+# loops until user enters maze
 beginning_loop:
     la $a0, enter_maze_msg   # Load the address of the enter maze message
     li $v0, 4             # Print the enter maze message
     syscall
 
-    ### Read inital character
+    # Read inital character
     addi $v0, $zero, 12 
 	syscall
-	move $t1, $v0
+	move $t4, $v0
 
-    beq $t1, 'R', start_maze
+    beq $t4, 'R', start_maze
 	j beginning_loop
 
-### informs user they have entered the maze and starts main loop
+# informs user they have entered the maze and starts main loop
 start_maze:
 	addi $v0, $zero, 4 
 	la $a0, maze_start_msg
@@ -68,7 +60,7 @@ start_maze:
 main_loop:
     # Get user input (direction)
     jal get_user_input
-    move $s2, $v0            # Store the user input (R, L, F, B) in $s2
+    move $t4, $v1            # Store the user input (R, L, F, B) in $t4
     
     # Update the robot's position based on the user input
 
@@ -81,32 +73,33 @@ main_loop:
     j main_loop                # Otherwise, continue with the main loop
     
 get_user_input:
-    # To do: Read user input (F, B, L, R)
-    li $v0, 12
+    # Read user input (F, B, L, R)
+    addi $v0, $zero, 12
     syscall
     move $v1, $v0
 
     # Check if the user input is valid (F, B, L, R)
-    li $t4, 'F'
-    beq $v1, $t4, valid_input
-    li $t4, 'B'
-    beq $v1, $t4, valid_input
-    li $t4, 'L'
-    beq $v1, $t4, valid_input
-    li $t4, 'R'
-    beq $v1, $t4, valid_input
+    li $t9, 'F'
+    beq $v1, $t9, valid_input
+    li $t9, 'B'
+    beq $v1, $t9, valid_input
+    li $t9, 'L'
+    beq $v1, $t9, valid_input
+    li $t9, 'R'
+    beq $v1, $t9, valid_input
 
+    # Invalid input: Show error message and repeat
     la $a0, input_error_msg
     li $v0, 4
     syscall
     j get_user_input
 
 valid_input:
-    move $v0, $v1
+    # Input is valid; proceed
     jr $ra
     
 update_position:
-    ## Update stack with return address
+    # Update stack with return address
     addi $sp, $sp, -8      # Adjust stack pointer
     sw $ra, 4($sp)         # Save return address
 
@@ -119,6 +112,11 @@ update_position:
     # Increment the total moves counter
     addi $s3, $s3, 1
     sw $s3, total_moves
+
+    # Restore return address and stack pointer
+    lw $ra, 4($sp)         # Restore return address
+    addi $sp, $sp, 8       # Adjust stack pointer back
+
     jr $ra
 
 decrease_xcord:
@@ -247,7 +245,7 @@ check_left:
     addi $sp, $sp, -8      # Adjust stack pointer
     sw $ra, 4($sp)         # Save return address
 
-    jal load_cell_values # Load beginning address of current cell
+    jal load_cell_address # Load beginning address of current cell
     
     lb $t6,3($t5)  # return the left direction value within current cell 
     li $t7, '1'  # load '1' into temporary register for later use (indeed, this variable can also be assigned at the begining)
@@ -276,7 +274,7 @@ invalid_move_forward:
     sw $s2, mistakes # save the number of mistakes to the 'mistakes' variable
 
     li $t8, 'B' # load the character 'B' into register $t8
-    beq $v1, $t8, main_loop # check if character inputed by the user is equal to 'B' to get unstuck
+    beq $v1, $t8, count_unstuck_move # check if character inputed by the user is equal to 'B' to get unstuck
 
     j invalid_move_forward # if 'B' is not entered run invalid_move_forward again
 
@@ -296,7 +294,7 @@ invalid_move_backwards:
     sw $s2, mistakes # save the number of mistakes to the 'mistakes' variable
 
     li $t8, 'F' # load the character 'F' into register $t8
-    beq $v1, $t8, main_loop # check if character inputed by the user is equal to 'F' to get unstuck
+    beq $v1, $t8, count_unstuck_move # check if character inputed by the user is equal to 'F' to get unstuck
 
     j invalid_move_backwards # if 'F' is not entered run invalid_move_backwards again
 
@@ -316,7 +314,7 @@ invalid_move_left:
     sw $s2, mistakes # save the number of mistakes to the 'mistakes' variable
 
     li $t8, 'R' # load the character 'R' into register $t8
-    beq $v1, $t8, main_loop # check if character inputed by the user is equal to 'R' to get unstuck
+    beq $v1, $t8, count_unstuck_move # check if character inputed by the user is equal to 'R' to get unstuck
 
     j invalid_move_left # if 'R' is not entered run invalid_move_left again
 
@@ -336,14 +334,14 @@ invalid_move_right:
     sw $s2, mistakes # save the number of mistakes to the 'mistakes' variable
 
     li $t8, 'L' # load the character 'L' into register $t8
-    beq $v1, $t8, main_loop # check if character inputed by the user is equal to 'R' to get unstuck
+    beq $v1, $t8, count_unstuck_move # check if character inputed by the user is equal to 'R' to get unstuck
 
     j invalid_move_right # if 'L' is not entered run invalid_move_right again
 
 load_cell_address:
     addi $sp, $sp, -8      # Adjust stack pointer
     sw $ra, 4($sp) 	
-    # TODO Load cell bitmask into $t7
+    # Load cell beginning address into $t5
     mul $t5, $t1, $s1  # Multiply y-coordinate by width of maze, save the result in $t5
     add $t5, $t5, $t0  # Add the result by x-coordinate, and get the index of the target cell on the 1-D arrary, save the result back to $t5
     mul $t5, $t5, 5  # Since each cell contains 5 digits (4 number and a ","), multiply the index by 5 to get the actual memory address offset, and save the offset value into $t5
@@ -353,11 +351,16 @@ load_cell_address:
     addi $sp, $sp, 8       # Adjust stack pointer back
     jr $ra
   
+count_unstuck_move:
+    addi $s3, $s3, 1 # increment for unstuck move
+    sw $s3, total_moves
+    j main_loop
 
 check_exit:
+    # Check if the robot reach to the exit, if so return 1 (exit condition)
     # Check if x = 5 and y = 0
-    li $t8, 5                  # load 5 into temp register
-    bne $t0, $t8, not_exit    # If x != 5, not exit
+    li $t9, 5                  # load 5 into temp register
+    bne $t0, $t9, not_exit    # If x != 5, not exit
     bne $t1, $zero, not_exit  # If y != 0, not exit
     li $v0, 1                 # Return 1 (exit found)
     jr $ra
